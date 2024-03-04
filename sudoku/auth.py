f60b32b3a4ae615ad86751effda30ba9aed0a576
hanrosec <anton48681@gmail.com>
2024-03-04 12:12:46 +0100

    Fixed error while providing not existing username

--- sudoku/auth.py
+++ sudoku/auth.py
@@ -15,9 +15,9 @@ bp = Blueprint('auth', __name__, url_prefix="/auth")
 
 def validate_username(username) -> bool:
     """
-    Nazwa użytkownika musi spełniać regex [A-Za-z0-9]. Przeciwdziałanie sql injection
+    Nazwa użytkownika musi spełniać regex [A-Za-z0-9] i mieć mniej niż 50 znaków. Przeciwdziałanie sql injection
     """
-    return bool(re.match(r"^\w+$", username)) and len(username) <= 50
+    return bool(re.match(r"^\w+$", username)) and len(username) < 50
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
@@ -25,7 +25,7 @@ def register():
         # pobranie nazwy uzytkownika i hasla z formularza do rejestracji
         username = request.form['username']
         password = request.form['password']
-        
+
         # inicjalizacja bazy danych
         db = get_db()
         error = None
@@ -36,20 +36,20 @@ def register():
             error = "Podaj hasło"
         
         if not validate_username(username):
-            error = "Nazwa użytkownika nie może zawierać tylko duże i małe litery oraz znak '_'. Może najwyżej 50 znaków"
-        
-        if error is None:
-            try:
-                db.execute(
-                    "INSERT INTO users (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password))
-                )
-                db.commit()
-            except db.IntegrityError:
-                error = f"Użytkownik o nazwie {username} jest już istnieje"
-            else:
-                # w przypadku poprawnego zarejestrowania uzytkownik jest przekierowywany na strone logowania
-                return redirect(url_for("auth.login"))
+            error = "Nazwa użytkownika nie może zawierać tylko duże i małe litery oraz znak _ oraz musi mieć długość mniejszą niż 50 znaków"
+        else:
+            if error is None:
+                try:
+                    db.execute(
+                        "INSERT INTO users (username, password) VALUES (?, ?)",
+                        (username, generate_password_hash(password))
+                    )
+                    db.commit()
+                except db.IntegrityError:
+                    error = f"Użytkownik o nazwie {username} jest już istnieje"
+                else:
+                    # w przypadku poprawnego zarejestrowania uzytkownik jest przekierowywany na strone logowania
+                    return redirect(url_for("auth.login"))
     
         flash(error)
         
@@ -60,29 +60,28 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        
         db = get_db()
         error = None
-                
+        user = None
+
         if not validate_username(username):
-            error = "Nazwa użytkownika nie może zawierać tylko duże i małe litery oraz znak _"
-        
-        if error is None:
-            user = db.execute(
-                'SELECT * FROM users WHERE username = ?', (username, )
-            ).fetchone()
+            error = "Nazwa użytkownika nie może zawierać tylko duże i małe litery oraz znak _ oraz musi mieć długość mniejszą niż 50 znaków"
+        else:
+            if error is None:
+                user = db.execute(
+                    'SELECT * FROM users WHERE username = ?', (username, )
+                ).fetchone()
+                
+            if user is None:
+                error = 'Niepoprawna nazwa użytkownika'
+            elif not check_password_hash(user['password'], password):
+                error = 'Niepoprawne hasło'
             
-        #! Przy za długiej nazwie użytkownika pojawia się błąd UnboundLocalError
-        if user is None:
-            error = 'Niepoprawna nazwa użytkownika'
-        elif not check_password_hash(user['password'], password):
-            error = 'Niepoprawne hasło'
-        
-        if error is None:
-            print(f"{user['id']} logged in")
-            session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('game.game'))
+            if error is None:
+                print(f"{user['id']} logged in")
+                session.clear()
+                session['user_id'] = user['id']
+                return redirect(url_for('game.game'))
         
         flash(error)
         

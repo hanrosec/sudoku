3489e77d9fc4587c2e4075c4c55aaf3c357f37fe
Hanrose4822 <anton48681@gmail.com>
2024-03-06 20:09:55 +0100

    deleted unnecessary comments and prints

--- sudoku/__init__.py
+++ sudoku/__init__.py
@@ -5,16 +5,12 @@ from flask import Flask, render_template
 def create_app(test_config=None):
     # Utworzenie i konfiguracja aplikacji
     app = Flask(__name__)
-    # app.config.from_mapping(
-    #     SECRET_KEY='dev',
-    #     DATABASE=os.path.join(app.instance_path, 'sudoku.sqlite'),
-    # )
     
     if test_config is None:
         app.config.from_pyfile('config.py', silent=False)
     else:
         app.config.from_mapping(test_config)
-    print(app.config)
+
     try:
         os.makedirs(app.instance_path)
     except OSError:
